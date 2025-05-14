6b35d819c72c9537d0a7bc525f3d0a1c835e7d2c
Snigdha Kantlam <kantlamsnigdha@gmail.com>
2025-05-14 18:10:47 +0530

    git commit -m "my second project"

--- B.java
+++ B.java
@@ -17,5 +17,12 @@ class B
 		System.out.println(-34452);
 		System.out.println(854);
 		System.out.println(-6547);
+
+		System.out.println(12);
+		System.out.println(1234);
+		System.out.println(0);
+		System.out.println(-34452);
+		System.out.println(854);
+		System.out.println(-6547);
 	}
 }
